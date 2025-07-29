e24529d8822c9d5638433e24e821cbcfec0bc069
feliperibeirofilg <felipearaujorib@icloud.com>
2025-07-28 21:14:04 -0300

    Estilizado tela de login e edição de usuario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,24 +2,27 @@
 
 @section('content')
 
+    <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+        
+        <form class="bg-white p-4 rounded shadow" style="width: 100%; max-width: 500px;" action="{{ route('logando') }}" method="Post">
+            @csrf
+            <h1 class="mb-4 text-center">Faça o seu login:</h1>
 
-    <h1>Faça o seu login:</h1>
-    <form class='form-control' action="{{ route('logando') }}" method="Post">
-        @csrf
-        <div class="form-control">
-            <label for="email">
-                <input type="text" name="email" placeholder="Digite o seu email:">
-            </label>
-        </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                    <input class="form-control border border-dark rounded" type="text" name="email" placeholder="Digite o seu email:">
+                
+            </div>
 
-        <div class="form-control">
-            <label for="password">
-                <input type="password" name="password" placeholder="Digite sua senha:">
-            </label>
-        </div>
-        <div>
-            <input class="btn btn-primary" type="submit" value="Login">
-        </div>
-    </form>
+            <div class="mb-3">
+                <label for="password" class="form-label">Senha:</label>
+                    <input class="form-control border border-dark rounded" type="password" name="password" placeholder="Digite sua senha:">
+                
+            </div>
+            <div class="text-center">
+                <input class="btn btn-primary" type="submit" value="Login">
+            </div>
+        </form>
+    </div>
 
 @endsection
